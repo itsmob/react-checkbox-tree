ff0153444758faee1b7bc2b41ac15126bbe29d90
itsmob <mobster.dev.96@gmail.com>
2023-08-16 15:16:44 -0400

    GO to the farest I get before to implement gary's solution

--- src/components/CheckboxTree.jsx
+++ src/components/CheckboxTree.jsx
@@ -1,24 +1,34 @@
 import { pathHasChildren, parseDecendants, descendantsAmount } from '../utils/checkboxTree';
+import { useState } from 'react';
 
-export default function CheckboxTree({ paths }) {
-  debugger;
+export default function CheckboxTree({ paths, parentCheck = true }) {
+  const [boxState, setBoxState] = useState(false);
+
   const jsx = [];
   let i = 0;
+
   while (i < paths.length) {
     if (!pathHasChildren(paths[i], paths[i + 1])) {
       jsx.push(
         <li key={paths[i]}>
-          <input type='checkbox' name={paths[i]} id={paths[i]} />
+          <input type='checkbox' name={paths[i]} id={paths[i]} disabled={!parentCheck} />
           <label htmlFor={paths[i]}>{paths[i]}</label>
         </li>,
       );
     } else {
-      const decendants = parseDecendants(paths, i);
       jsx.push(
         <li key={paths[i]}>
-          <input type='checkbox' name={paths[i]} id={paths[i]} />
+          <input
+            type='checkbox'
+            name={paths[i]}
+            id={paths[i]}
+            onChange={() => {
+              setBoxState(!boxState);
+            }}
+            disabled={!parentCheck}
+          />
           <label htmlFor={paths[i]}>{paths[i]}</label>
-          <CheckboxTree paths={decendants} />
+          <CheckboxTree paths={parseDecendants(paths, i)} parentCheck={boxState} />
         </li>,
       );
       i = i + descendantsAmount(paths, i);
